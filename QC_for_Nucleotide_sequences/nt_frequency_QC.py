9252911313323255fe4578b618924a365b05e69d
juanledesma78 <63557623+juanledesma78@users.noreply.github.com>
2020-04-26 18:16:09 +0100

    Update and rename Nt_frequency_calculator.py to nt_frequency_QC.py

--- QC_for_Nucleotide_sequences/nt_frequency_QC.py
+++ QC_for_Nucleotide_sequences/nt_frequency_QC.py
@@ -2,13 +2,15 @@ import sys
 inFile = sys.argv[1]
 #cutoff = sys.argv[2] #in case the cutoff is taken as argument at the time of running the script
 input_file = open(inFile, 'rt')
-output_name = inFile.replace('.fas','_Nucleotides_composition_QC.txt')
-QC_summary= open(output_name,'wt')
+QC_name = inFile.replace('.fas','_Nucleotides_composition_QC.txt')
+FASTA_name = inFile.replace('.fas','_QC_passed.fas')
+QC_summary = open(QC_name,'wt')
+FASTA_selected = open(FASTA_name,'wt')
 
 seq_line=""
 headers=[]
 sequences=[]
-comments = '\n'+'WARNING!'+'\n'+'The frequency of undetermined nucleotides (Ns) in the following sequences is higher than 20% so they will be excluded from further analysis:'+('\n')*2
+comments = '\n'+'WARNING!'+'\n'+'WARNING!'+'\n'+'The frequency of undetermined nucleotides (Ns) in the following sequences is higher than 20% so they will be excluded from further analysis'+('\n')*2
 for line in input_file:
     if ">" in line:
         hdr=(line.strip('>')).rstrip()
@@ -21,8 +23,9 @@ for line in input_file:
         seq_line+=y.upper() 
 sequences.append(seq_line)
 
+
 IUPAC_nts = ['A', 'C', 'G', 'T', 'U', 'W', 'S', 'M', 'K', 'R', 'Y' , 'B', 'D', 'H', 'V', 'N']
-seqs_QC_not_passed = [] #kept to update the script with a tool to select the fasta sequences passing the QC
+seq_QC_passed = ""
 for i in range(len(headers)):
     title_columns= headers[i] +'\t'+'n'+'\t' + 'Frequency'+ '\n'
     QC_summary.write(title_columns)
@@ -35,12 +38,16 @@ for i in range(len(headers)):
             QC_summary.write(nt_info)
             if IUPAC_nts[x] =='N':
                 #if nt_PC > int(cutoff):
+                if nt_PC <= 20:
+                    h = '>'+ headers[i] + '\n'+ sequences[i] + '\n'
+                    seq_QC_passed += h
                 if nt_PC > 20:
                     qc=headers[i] + '\t'+ str("%.2f" % round(nt_PC,2)) +'%' + '\n'
                     comments +=qc
-                    seqs_QC_not_passed.append(headers[i])
                 
     QC_summary.write('lentgh' + '\t'+ str(genome_length) + '\n'+ '\n')
+FASTA_selected.write(seq_QC_passed)
 QC_summary.write(comments)
 QC_summary.close()
+FASTA_selected.close()
 input_file.close()
